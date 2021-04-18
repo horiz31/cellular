0a73f694f535874ebaac4d7d49fb86d02c89bbfb
ze8 <stinsonbj@ornl.gov>
2021-04-18 15:36:10 -0400

    remove comment

--- cellular-start.sh
+++ cellular-start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #bringup cellular, tested with Sierra Wireless M.2 modems which only support raw-ip
-#todo, make APN an parameter in /etc/systemd/cellular.conf
-
 
 ifconfig wwan0 down
 echo Y > /sys/class/net/wwan0/qmi/raw_ip
