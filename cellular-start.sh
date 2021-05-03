b1a8c3c43a1046b45b7862a49e21fb76222b84e6
Brad Stinson <brad@horizon31.com>
2021-05-03 18:15:44 -0400

    use error codes to make sure service retries

--- cellular-start.sh
+++ cellular-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #bringup cellular, tested with Sierra Wireless M.2 modems which only support raw-ip
-
+set -e
 ifconfig wwan0 down
 echo Y > /sys/class/net/wwan0/qmi/raw_ip
 ifconfig wwan0 up
 qmicli -d /dev/cdc-wdm0 --device-open-net="net-raw-ip|net-no-qos-header" --wds-start-network="apn='${APN}',ip-type=4" --client-no-release-cid 
 udhcpc -i wwan0 -q -T 20
-
+set +e
