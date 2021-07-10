6b55615ddb70f965e04b634957ee3e73a77d4b73
climr <stinsonbj@ornl.gov>
2021-07-10 12:34:18 -0400

    Update cellular-start.sh

--- cellular-start.sh
+++ cellular-start.sh
@@ -5,6 +5,9 @@ set -e
 ifconfig wwan0 down
 echo Y > /sys/class/net/wwan0/qmi/raw_ip
 ifconfig wwan0 up
-qmicli -d /dev/cdc-wdm0 --device-open-net="net-raw-ip|net-no-qos-header" --wds-start-network="apn='${APN}',ip-type=4" --client-no-release-cid 
+RET=$(qmicli -d /dev/cdc-wdm0 --device-open-net="net-raw-ip|net-no-qos-header" --wds-start-network="apn='${APN}',ip-type=4" --client-no-release-cid)
+#save the network handle and CID
+NETWORK_HANDLE=$(echo $RET | cut -f2 -d\' | xargs)
+CID=$(echo $RET | cut -f5 -d: | xargs)
 udhcpc -i wwan0 -q -T 20
 set +e
