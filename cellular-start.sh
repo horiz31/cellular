dcefa0edba08937d3a9635501578a21103a30b2b
Brad Stinson <brad@horizon31.com>
2021-07-11 10:50:53 -0400

    rework to handle retries and failures better

--- cellular-start.sh
+++ cellular-start.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 ENV_LOCATION=/var/run/cellular
+TMP_FILE=/tmp/cellular.$$
+
 #bringup cellular, tested with Sierra Wireless M.2 modems which only support raw-ip
-set -e
 #set raw-ip mode, needed by sierra wireless modems tested with
 ifconfig wwan0 down
 echo Y > /sys/class/net/wwan0/qmi/raw_ip
 ifconfig wwan0 up
-qmicli -d /dev/cdc-wdm0 --device-open-net="net-raw-ip|net-no-qos-header" --wds-start-network="apn='${APN}',ip-type=4" --client-no-release-cid > /tmp/cellular.$$
+#try to start the network
+qmicli -d /dev/cdc-wdm0 --device-open-net="net-raw-ip|net-no-qos-header" --wds-start-network="apn='${APN}',ip-type=4" --client-no-release-cid > $TMP_FILE
+if [ -f $TMP_FILE ]; then
+   if ! grep -q "Network started" $TMP_FILE ; then
+	echo "Network not started, exiting..."
+	exit 1
+   fi
+else
+   echo "Error starting cellular, exiting..."
+   exit 1
+fi
+
+#if we got this far, then the network started
 #make environment file
 touch /var/run/cellular/cellular.env
+echo "Start success, response:"
 cat /tmp/cellular.$$
+
 NETWORK_HANDLE=$(cat /tmp/cellular.$$ | cut -f2 -d\' |  head -2 | tail -1 | xargs)
 CID=$(cat /tmp/cellular.$$ | tail -1 | cut -f2 -d: | xargs)
 echo "NETWORK HANDLE is $NETWORK_HANDLE"
@@ -18,7 +33,7 @@ echo "CID is $CID"
 echo "NETWORK_HANDLE=${NETWORK_HANDLE}" >> ${ENV_LOCATION}/cellular.env && \
 echo "CID=${CID}" >> ${ENV_LOCATION}/cellular.env
 #ask for dhcp address
-udhcpc -i wwan0 -q -T 20
+if ! udhcpc -i wwan0 -n -q -t 3 -T 3 ; then exit 1 ; fi
 #cleanup temp files
 rm /tmp/cellular.$$
-set +e
+
--- cellular-stop.sh
+++ cellular-stop.sh
@@ -2,16 +2,17 @@
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 ENV_LOCATION=/var/run/cellular
 #shutdown cellular
-set -e
 #get the network handle and cid from file
-NETWORK_HANDLE=$($SUDO grep ^NETWORK_HANDLE ${ENV_LOCATION}/cellular.env 2>/dev/null | cut -f2 -d=)
-CID=$($SUDO grep ^CID ${ENV_LOCATION}/cellular.env 2>/dev/null | cut -f2 -d=)
+if [ -f "${ENV_LOCATION}/cellular.env" ] ; then
+   NETWORK_HANDLE=$($SUDO grep ^NETWORK_HANDLE ${ENV_LOCATION}/cellular.env 2>/dev/null | cut -f2 -d=)
+   CID=$($SUDO grep ^CID ${ENV_LOCATION}/cellular.env 2>/dev/null | cut -f2 -d=)
+   #release network
+   echo "Shutting down cellular Network ${NETWORK_HANDLE} CID ${CID}..."  
+   qmicli -d /dev/cdc-wdm0 --wds-stop-network=$NETWORK_HANDLE --client-cid=$CID
+else
+   echo "Envrionment file not found, not a graceful shutdown..."
+fi
 
-echo "Shutting down cellular.."
-echo "NETWORK_HANDLE is ${NETWORK_HANDLE}"
-echo "CID is ${CID}"
-#release network
-qmicli -d /dev/cdc-wdm0 --wds-stop-network=$NETWORK_HANDLE --client-cid=$CID
 #take down the interface
 ifconfig wwan0 down
-set +e
+
